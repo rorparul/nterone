bd76ec40ec1776b51ba41f4b58a8fa03e6292078
Ryan Storberg <ryan@storberg.net>
2016-02-08 23:52:35 -0500

    Remove unused assets; update README; comment out asset precompilation in application.rb file

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module NterOne
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w( brands.js general.js rules.js subjects.js )
+    # config.assets.precompile += %w( brands.js general.js rules.js subjects.js )
     config.active_job.queue_adapter = :sidekiq
   end
 end
