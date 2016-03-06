a8ba85e66676d542585ca1cf3354d05241e61d5c
Ryan Storberg <ryan@storberg.net>
2016-03-06 16:46:03 -0500

    Fix login requirement to view vendor category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: :show
   before_action :set_category, only: [:show, :update, :destroy]
 
   def index
