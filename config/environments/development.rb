3bc5765905dcddd5e04c0d4b8f5a7da692c8286c
Ryan Storberg <ryan@storberg.net>
2016-03-12 11:14:32 -0500

    Add font-awesome locally; in the middle of exanding forums

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,7 +44,6 @@ NterOne::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.raise_delivery_errors = true
