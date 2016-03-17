bea5a9276681d4af5252027c7884035c013be92d
Ryan Storberg <ryan@storberg.net>
2016-03-16 21:31:46 -0400

    Troubleshoot checkout

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -4,9 +4,9 @@ module CurrentCart
   private
 
   def set_cart
-    @cart = Cart.find(session[:cart_id])
+    @cart = Cart.find(cookies[:cart_id])
   rescue ActiveRecord::RecordNotFound
     @cart = Cart.create
-    session[:cart_id] = @cart.id
+    cookies[:cart_id] = @cart.id
   end
 end
