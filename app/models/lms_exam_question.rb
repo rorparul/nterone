fed65752b02676c749ed0095d92277540a124604
Alan Cohengit <alan.cohen@nyu.edu>
2016-05-02 20:28:24 -0400

    added correct_answer to lms_exam_questions

--- app/models/lms_exam_question.rb
+++ app/models/lms_exam_question.rb
@@ -3,6 +3,5 @@ class LmsExamQuestion < ActiveRecord::Base
 
   has_many :lms_exam_question_joins
   has_many :lms_exams, through: :lms_exam_question_joins
-
-  has_one :lms_exam_answer
+  has_many :lms_exam_answer
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160502163121) do
+ActiveRecord::Schema.define(version: 20160503002245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -433,8 +433,9 @@ ActiveRecord::Schema.define(version: 20160502163121) do
   create_table "lms_exam_questions", force: :cascade do |t|
     t.text     "question_text"
     t.integer  "question_type"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "correct_answer_id"
   end
 
   create_table "lms_exams", force: :cascade do |t|
