279785089b38d3083b2f0bf90eb696ae1c8725b3
Alan Cohengit <alan.cohen@nyu.edu>
2016-05-02 20:47:22 -0400

    added association for correct_answer

--- app/models/lms_exam_question.rb
+++ app/models/lms_exam_question.rb
@@ -4,4 +4,6 @@ class LmsExamQuestion < ActiveRecord::Base
   has_many :lms_exam_question_joins
   has_many :lms_exams, through: :lms_exam_question_joins
   has_many :lms_exam_answers
+
+  belongs_to :correct_answer, class_name: "LmsExamAnswer"
 end
