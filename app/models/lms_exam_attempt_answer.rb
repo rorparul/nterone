41655c98af82291cb5e7981dcb7d5e532bb383de
Alan Cohengit <alan.cohen@nyu.edu>
2016-05-05 20:24:21 -0400

    quiz summary highlights user answer and correct answer

--- app/models/lms_exam_attempt_answer.rb
+++ app/models/lms_exam_attempt_answer.rb
@@ -2,4 +2,8 @@ class LmsExamAttemptAnswer < ActiveRecord::Base
   belongs_to :lms_exam_attempt
   belongs_to :lms_exam_question
   belongs_to :lms_exam_answer
+
+  def correct_answer
+    lms_exam_question.lms_exam_answers.where(correct: true)
+  end
 end
