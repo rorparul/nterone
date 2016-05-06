bb49c6dc68eff2d504c4129a502917672f648b9a
Alan Cohengit <alan.cohen@nyu.edu>
2016-05-05 21:39:14 -0400

    refactored score calculations

--- app/models/lms_exam_attempt.rb
+++ app/models/lms_exam_attempt.rb
@@ -3,4 +3,14 @@ class LmsExamAttempt < ActiveRecord::Base
   belongs_to :user
 
   has_many :lms_exam_attempt_answers
+
+  def number_correct
+    num_correct = 0
+
+    lms_exam_attempt_answers.each do |attempt_answer|
+      num_correct += 1 if attempt_answer.lms_exam_answer.correct
+    end
+
+    num_correct
+  end
 end
