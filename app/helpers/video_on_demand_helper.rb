ffb33e9ccb78463e4405e52074d4dada4b70723b
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-05-12 09:54:35 +0800

    style changes to VOD header

--- app/helpers/video_on_demand_helper.rb
+++ app/helpers/video_on_demand_helper.rb
@@ -5,7 +5,7 @@ module VideoOnDemandHelper
 
   def video_completed_class(video_module, user)
     return '' if video_module.videos.count == 0
-    video_module.watched_count(user) == video_module.videos.count ? 'text-success' : ''
+    video_module.watched_count(user) == video_module.videos.count ? 'label-success' : 'label-default'
   end
 
   def exam_completed_ratio(video_module, user)
@@ -14,6 +14,6 @@ module VideoOnDemandHelper
 
   def exam_completed_class(video_module, user)
     return '' if video_module.exams_count == 0
-    video_module.completed_exams_count_for(current_user) == video_module.exams_count ? 'text-success' : ''
+    video_module.completed_exams_count_for(current_user) == video_module.exams_count ? 'label-success' : 'label-default'
   end
 end
