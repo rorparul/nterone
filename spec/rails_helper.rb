9b2e313c6c5cba52e7e2b9218a34aa34a64ff3cb
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-06-03 09:39:56 +0400

    video model test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,10 +13,6 @@ Dir[File.expand_path(File.join(File.dirname(__FILE__),"support","**","*.rb"))].e
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.include Rails::Controller::Testing::TestProcess
-  config.include Rails::Controller::Testing::TemplateAssertions
-  config.include Rails::Controller::Testing::Integration
-
   config.infer_spec_type_from_file_location!
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
