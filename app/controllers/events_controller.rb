24bdf55a91d682d71df406e8549048429dca3c24
Ryan Storberg <ryan@storberg.net>
2016-06-22 20:24:48 -0400

    Temporarily comment out 'EventReminderWorker'

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -65,7 +65,7 @@ class EventsController < ApplicationController
     @course   = Course.find(params[:course_id])
     @event    = Event.find(params[:id])
 
-    EventReminderWorker.perform
+    # EventReminderWorker.perform # TODO: Undo this comment after fix
 
     if @event.update_attributes(event_params)
       flash[:success] = 'Event successfully updated!'
