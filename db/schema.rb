f5238fb97bd94b9fc8e700ff12c3873f286d0826
Ryan Storberg <ryan@storberg.net>
2016-06-28 15:49:28 -0400

    Add rails-settings-cache gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628194305) do
+ActiveRecord::Schema.define(version: 20160628194617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -637,6 +637,17 @@ ActiveRecord::Schema.define(version: 20160628194305) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "settings", force: :cascade do |t|
+    t.string   "var",                   null: false
+    t.text     "value"
+    t.integer  "thing_id"
+    t.string   "thing_type", limit: 30
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "settings", ["thing_type", "thing_id", "var"], name: "index_settings_on_thing_type_and_thing_id_and_var", unique: true, using: :btree
 
   create_table "subject_groups", force: :cascade do |t|
     t.integer  "subject_id"
