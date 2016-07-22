208b16e0635a9410a398921e557f9fe347cc4749
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-07-22 16:43:46 +0400

    commission report generate xlsx

--- app/controllers/generate_commission_reports_controller.rb
+++ app/controllers/generate_commission_reports_controller.rb
@@ -6,6 +6,16 @@ class GenerateCommissionReportsController < ApplicationController
   end
 
   def create
+    @orders = Order.events_in_range_for(*report_params.values)
 
+    respond_to do |format|
+      format.xlsx
+    end
+  end
+
+private
+
+  def report_params
+    params.require(:report).permit(:sales_rep, :start_date, :end_date)
   end
 end
