c13bf23ebf610f4ca39f396935418c30af1fe16f
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-07-28 23:22:17 +0400

    fixed totals

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,7 @@
 module OrdersHelper
-  def orders_revenue_total(order_items)
+  def orders_revenue_total(order_items, rep_id)
     order_items.inject(0) do |items_sum, item|
-      item.orderable_type == 'Event' ? item.orderable.revenue + items_sum : items_sum
+      item.orderable_type == 'Event' ? item.orderable.revenue_by(rep_id) + items_sum : items_sum
     end
   end
 
