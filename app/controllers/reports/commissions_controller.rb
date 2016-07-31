5ce19479e99e46ab5e54ecc39747f1ae2a038eb8
Davit Khaburdzania <davit.khaburdzania@gmail.com>
2016-07-31 15:42:29 +0400

    sales rep should generate only his commission reports

--- app/controllers/reports/commissions_controller.rb
+++ app/controllers/reports/commissions_controller.rb
@@ -10,7 +10,7 @@ class Reports::CommissionsController < ApplicationController
     end_date = parse_date_select(report_params, :end_date)
 
     @order_items  = Order.items_in_range_for(report_params[:sales_rep], start_date, end_date)
-    @sales_rep_id = report_params[:sales_rep]
+    @sales_rep_id = current_user.sales_rep? ? current_user.id : report_params[:sales_rep]
 
     respond_to do |format|
       format.xlsx
