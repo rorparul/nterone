0b07f0a74d9c50ca94f55418350945e02e61b888
Ryan Storberg <ryan@storberg.net>
2016-08-07 16:06:42 -0400

    Add new server to capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :branch, 'master'
 
 server '184.7.26.58', user: fetch(:user), roles: %w{app db web}
+server '184.7.26.56', user: fetch(:user), roles: %w{app db web}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,3 @@
 set :branch, 'staging'
 
-server "184.7.18.90", user: fetch(:user), roles: %w(app db web)
+server '184.7.18.90', user: fetch(:user), roles: %w(app db web)
