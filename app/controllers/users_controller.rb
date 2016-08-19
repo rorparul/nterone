faad081f1c3469e5de7a73f54733dedc0d43a822
Ryan Storberg <ryan@storberg.net>
2016-08-19 17:07:33 -0400

    Fix issue where company ID is being saved to comapny_name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:company_name,
+    params.require(:user).permit(:company_id,
                                  :first_name,
                                  :last_name,
                                  :contact_number,
