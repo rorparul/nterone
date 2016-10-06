0c72833af209a79e038753dc8c8602a639fcf507
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-10-06 13:23:06 -0400

    partially developed ActionMailer::Previews and fixed display issues in various mailers

--- test/mailers/previews/lab_reservation_mailer_preview.rb
+++ test/mailers/previews/lab_reservation_mailer_preview.rb
@@ -5,4 +5,18 @@ class LabReservationMailerPreview < ActionMailer::Preview
     reservation = LabRental.first
     LabReservationMailer.create_reservation(user, reservation)
   end
+
+  def update_reservation
+    user = User.first
+    user_email = user.email
+    reservation = LabRental.first
+    LabReservationMailer.update_reservation(user, reservation)
+  end
+
+  def destroy_reservation
+    user = User.first
+    user_email = user.email
+    reservation = LabRental.first
+    LabReservationMailer.destroy_reservation(user, reservation)
+  end
 end
