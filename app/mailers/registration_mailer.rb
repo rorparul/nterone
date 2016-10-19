f68d252a077f1579eabb1e8c8bb30e57d2ce7040
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-10-19 12:16:44 -0400

    reconfigured registration email

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,13 +1,13 @@
 class RegistrationMailer < ApplicationMailer
-  def registration_made(target_email, seller, user, event)
+  def registration_made(seller, user, event)
     @user                          = user
     @event                         = event
-    @seller_email                  = seller.email if seller
+    @seller_email                  = seller.email
     attachments.inline["logo.png"] = File.read(Rails.root.join("app/assets/images/logo.png"))
 
     mail(
-      to: target_email,
-      bcc: ["ashlie#{I18n.t('email')}", "leslie#{I18n.t('email')}", @seller_email],
+      to: @seller_email,
+      bcc: ["ashlie#{I18n.t('email')}", "leslie#{I18n.t('email')}"],
       subject:       'NterOne Web Student Added Confirmation',
       template_path: 'mailers',
       template_name: 'student_added'
