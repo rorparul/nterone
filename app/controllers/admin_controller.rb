d8ab1cacd90a7ad2803990fa191376d26adf0997
Ryan Storberg <ryan@storberg.net>
2016-11-15 17:34:17 -0500

    Merge master

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -37,12 +37,12 @@ class AdminController < ApplicationController
   end
 
   def classes
-    cookies[:including_past]  = params[:including_past]   if params[:including_past]
-    cookies[:only_registered] = params[:only_registered]  if params[:only_registered]
-    cookies[:filter]          = params[:filter]           if params[:filter]
+    cookies[:including_past]  = params[:including_past]  if params[:including_past]
+    cookies[:only_registered] = params[:only_registered] if params[:only_registered]
+    cookies[:filter]          = params[:filter]          if params[:filter]
 
     events_scope = cookies[:including_past] == "1" ? Event.joins(:course) : Event.joins(:course).upcoming_events
-    events_scope = events_scope.with_students                  if cookies[:only_registered] == "1" || cookies[:only_registered].blank?
+    events_scope = events_scope.with_students if cookies[:only_registered] == "1" || cookies[:only_registered].blank?
     events_scope = events_scope.custom_search(cookies[:filter]) if cookies[:filter]
 
     @queried_events = events_scope
