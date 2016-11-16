63855747c90a4830ce2fd931e2048b6b8b1a49f7
Ryan Storberg <ryan@storberg.net>
2016-11-16 16:42:24 -0500

    Remove filtering of carts

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -6,7 +6,8 @@ class InsightsController < ApplicationController
 
   def carts
     authorize :insight, :carts?
-    carts_scope = params[:including_empty] == "1" ? Cart.includes(:order_items) : Cart.joins(:order_items)
+    carts_scope = Cart.includes(:order_items)
+    # carts_scope = params[:including_empty] == "1" ? Cart.includes(:order_items) : Cart.joins(:order_items)
     # carts_scope = carts_scope.search_without_items(params[:filter]) if params[:filter]
     # if params[:including_empty] == "1"
     #   carts_scope = Cart.includes(:order_items)
