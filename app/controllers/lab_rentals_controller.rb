0abbcaafbc2dd8cc0db88718c159b6d35c9085dc
Oleg A. Bavaev <jesoba7@gmail.com>
2016-12-06 18:11:24 +1000

    Normalize indentation

--- app/controllers/lab_rentals_controller.rb
+++ app/controllers/lab_rentals_controller.rb
@@ -1,78 +1,78 @@
 class LabRentalsController < ApplicationController
-	include SmartListing::Helper::ControllerExtensions
+  include SmartListing::Helper::ControllerExtensions
   helper  SmartListing::Helper
 
   before_action :set_lab_rental, only: [:show, :edit, :update, :destroy]
 
   def index
-		lab_rentals_scope  = current_user.admin? ? LabRental.joins(:company).all : LabRental.where(company_id: current_user.company_id)
-		lab_rentals_scope  = LabRental.custom_search(params[:filter]) if params[:filter]
-		@lab_rentals       = smart_listing_create(
-			:lab_rentals,
-			lab_rentals_scope,
-			partial: "lab_rentals/listing",
-			sort_attributes: [[:course, "course"],
-												[:company, "companies.title"],
-												[:num_of_students, "num_of_students"],
-												[:first_day, "first_day"],
-												[:instructor, "instructor"],
-												[:location, "location"],
-											  [:confirmed, "confirmed"],
-											  [:canceled, "canceled"],
-											  [:twenty_four_hours, "twenty_four_hours"]],
-			default_sort: { "first_day": "desc" }
-		)
+    lab_rentals_scope  = current_user.admin? ? LabRental.joins(:company).all : LabRental.where(company_id: current_user.company_id)
+    lab_rentals_scope  = LabRental.custom_search(params[:filter]) if params[:filter]
+    @lab_rentals       = smart_listing_create(
+      :lab_rentals,
+      lab_rentals_scope,
+      partial: "lab_rentals/listing",
+      sort_attributes: [[:course, "course"],
+			[:company, "companies.title"],
+			[:num_of_students, "num_of_students"],
+			[:first_day, "first_day"],
+			[:instructor, "instructor"],
+			[:location, "location"],
+			[:confirmed, "confirmed"],
+			[:canceled, "canceled"],
+			[:twenty_four_hours, "twenty_four_hours"]],
+    default_sort: { "first_day": "desc" }
+    )
 
-		render layout: "admin" if current_user.admin?
-	end
+    render layout: "admin" if current_user.admin?
+  end
 
-	def show
-		if params[:show] == 'notes'
-			@note = @lab_rental.notes
-			render 'show_note'
-		elsif params[:show] == 'students'
-			@lab_students = @lab_rental.lab_students
-			render 'show_lab_students'
-		else
-			render nothing: true
-		end
-	end
+  def show
+    if params[:show] == 'notes'
+      @note = @lab_rental.notes
+      render 'show_note'
+    elsif params[:show] == 'students'
+      @lab_students = @lab_rental.lab_students
+      render 'show_lab_students'
+    else
+      render nothing: true
+    end
+  end
 
-	def new
-		company = Company.find_by(slug: params[:company_slug]) if params[:company_slug]
-		company ||= Company.find_by(id: params[:company_id])   if params[:company_id]
-		company ||= current_user.company                       if current_user && current_user.admin? == false
-		kind    = company.try(:form_type) ? company.form_type : 1
+  def new
+    company = Company.find_by(slug: params[:company_slug]) if params[:company_slug]
+    company ||= Company.find_by(id: params[:company_id])   if params[:company_id]
+    company ||= current_user.company                       if current_user && current_user.admin? == false
+    kind    = company.try(:form_type) ? company.form_type : 1
 
-		@lab_rental = LabRental.new(user_id: current_user.try(:id), company_id: company.try(:id), kind: kind)
-		@lab_rental.lab_students.build if @lab_rental.kind == 2
-	end
+    @lab_rental = LabRental.new(user_id: current_user.try(:id), company_id: company.try(:id), kind: kind)
+    @lab_rental.lab_students.build if @lab_rental.kind == 2
+  end
 
-	def new_file
+  def new_file
 
-	end
+  end
 
-	def upload
-		if current_user && current_user.admin?
-			upload = LabsUploader.upload(lab_rental_params[:file])
-		end
-		redirect_to :back
-	end
+  def upload
+    if current_user && current_user.admin?
+      upload = LabsUploader.upload(lab_rental_params[:file])
+    end
+    redirect_to :back
+  end
 
-	def create
-		@lab_rental = LabRental.new(lab_rental_params)
-		if @lab_rental.save
-			flash[:success] = 'Lab Reservation successfully submitted!'
-			LabReservationMailer.create_reservation(current_user, @lab_rental).deliver_now
-			if user_signed_in? && (current_user.admin? || current_user.company)
-				redirect_to lab_rentals_path
-			else
-				redirect_to :back
-			end
-		else
-			render 'new'
-		end
-	end
+  def create
+    @lab_rental = LabRental.new(lab_rental_params)
+    if @lab_rental.save
+      flash[:success] = 'Lab Reservation successfully submitted!'
+      LabReservationMailer.create_reservation(current_user, @lab_rental).deliver_now
+      if user_signed_in? && (current_user.admin? || current_user.company)
+	redirect_to lab_rentals_path
+      else
+	redirect_to :back
+      end
+    else
+      render 'new'
+    end
+  end
 
   def edit
   end
@@ -82,55 +82,55 @@ class LabRentalsController < ApplicationController
 
     if @lab_rental.save
       flash[:success] = 'Lab Reservation successfully updated!'
-			LabReservationMailer.update_reservation(current_user, @lab_rental).deliver_now
+      LabReservationMailer.update_reservation(current_user, @lab_rental).deliver_now
       redirect_to lab_rentals_path
     else
       render "edit"
     end
   end
 
-	def destroy
-		unless current_user.admin? == false && (@lab_rental.first_day - Date.today).to_i < 14
-			@lab_rental.update(canceled: true)
-			flash[:success] = 'Lab Reservation successfully canceled.'
-			LabReservationMailer.destroy_reservation(current_user, @lab_rental).deliver_now
-		else
-			flash[:alert] = "Lab Reservations can't be canceled within 2 weeks of their start date."
-		end
-		redirect_to :back
-	end
+  def destroy
+    unless current_user.admin? == false && (@lab_rental.first_day - Date.today).to_i < 14
+      @lab_rental.update(canceled: true)
+      flash[:success] = 'Lab Reservation successfully canceled.'
+      LabReservationMailer.destroy_reservation(current_user, @lab_rental).deliver_now
+    else
+      flash[:alert] = "Lab Reservations can't be canceled within 2 weeks of their start date."
+    end
+    redirect_to :back
+  end
 
-	private
+  private
 
   def set_lab_rental
     @lab_rental = LabRental.find(params[:id])
   end
 
-	def verify_company
-		redirect_to root_path if (!current_user.admin? && !current_user.company)
-	end
+  def verify_company
+    redirect_to root_path if (!current_user.admin? && !current_user.company)
+  end
 
-	def lab_rental_params
+  def lab_rental_params
     params.require(:lab_rental).permit(
-			:user_id,
-			:company_id,
-			:lab_course_id,
-     	:first_day,
-			:last_day,
-     	:num_of_students,
+      :user_id,
+      :company_id,
+      :lab_course_id,
+      :first_day,
+      :last_day,
+      :num_of_students,
       :start_time,
-     	:end_time,
-     	:instructor,
-     	:instructor_email,
-     	:instructor_phone,
-     	:notes,
-     	:location,
-     	:confirmed,
-			:kind,
-			:time_zone,
-			:twenty_four_hours,
-			:file,
-			lab_students_attributes: [:id, :name, :email, :_destroy]
-		)
+      :end_time,
+      :instructor,
+      :instructor_email,
+      :instructor_phone,
+      :notes,
+      :location,
+      :confirmed,
+      :kind,
+      :time_zone,
+      :twenty_four_hours,
+      :file,
+      lab_students_attributes: [:id, :name, :email, :_destroy]
+    )
   end
 end
