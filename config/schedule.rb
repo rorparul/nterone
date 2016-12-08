5da7cf18c93488b9d98f36af22d7d570528f1e7b
Oleg A. Bavaev <jesoba7@gmail.com>
2016-12-08 13:48:09 +1000

    Add task staging_db_clone

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,14 @@
 #   rake "-s sitemap:refresh"
 # end
 
-every 1.day, :at => '1:00 am' do
-  rake 'backup:db'
+if Setting.stage == 'production'
+  every 1.day, :at => '1:00 am' do
+    rake 'backup:db'
+  end
+end
+
+if Setting.stage == 'staging'
+  every 1.day, :at => '2:00 am' do
+    rake 'staging:db_clone'
+  end
 end
