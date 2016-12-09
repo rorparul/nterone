aac400c9b856dacae3135e09b8ee6cc343408b4b
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-12-09 17:27:07 -0500

    rake db:migrate

--- app/models/event.rb
+++ app/models/event.rb
@@ -37,12 +37,13 @@
 #  note                           :text
 #  count_weekends                 :boolean          default(FALSE)
 #  in_house_note                  :text
-#  street                         :string
 #  language                       :integer          default(0)
+#  street                         :string
 #  calculate_book_costs           :boolean          default(TRUE)
 #  autocalculate_instructor_costs :boolean          default(TRUE)
 #  resell                         :boolean          default(FALSE)
 #  zipcode                        :string
+#  company                        :string
 #
 
 class Event < ActiveRecord::Base
