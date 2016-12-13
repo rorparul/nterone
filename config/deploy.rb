0de7b9d130cbe6845b9d883bd18729f57ed10c91
Oleg A. Bavaev <jesoba7@gmail.com>
2016-12-13 13:09:36 +1000

    Add task deploy:db:load
    To load schema

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,3 +33,23 @@ namespace :deploy do
     end
   end
 end
+
+namespace :deploy do
+  namespace :db do
+    desc "Load the database schema if needed"
+    task load: [:set_rails_env] do
+      on primary :db do
+        if not test(%Q[[ -e "#{shared_path.join(".schema_loaded")}" ]])
+          within release_path do
+            with rails_env: fetch(:rails_env) do
+              execute :rake, "db:schema:load"
+              execute :touch, shared_path.join(".schema_loaded")
+            end
+          end
+        end
+      end
+    end
+  end
+
+  before "deploy:migrate", "deploy:db:load"
+end
