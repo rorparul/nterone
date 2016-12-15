b48da4c56abae3fd384a6ca09fd0fd09beb1617d
Ryan Storberg <ryan@storberg.net>
2016-12-15 10:11:44 -0500

    Add Canadian server to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,3 +2,4 @@ set :branch, 'master'
 
 server '184.7.26.58', user: fetch(:user), roles: %w{app db web}
 server '184.7.26.56', user: fetch(:user), roles: %w{app db web}
+server '184.7.18.89', user: fetch(:user), roles: %w{app db web}
--- config/initializers/dumper.rb
+++ config/initializers/dumper.rb
@@ -6,6 +6,6 @@ Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
   Rails.env.production? && Socket.gethostname == 'nterone.la'
 end
 
-# Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
-#   Rails.env.production? && Socket.gethostname == 'www.nterone.ca'
-# end
+Dumper::Agent.start_if(app_key: 'UnBZTswiW0ZTVu4EWww1Gz') do
+  Rails.env.production? && Socket.gethostname == 'nterone-ca'
+end
