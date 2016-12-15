b6312c9175dcb84c2bdbca6de8466575e8b36a6c
CsolG <csolg7@gmail.com>
2016-12-16 02:25:12 +1000

    Update schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,14 +2,10 @@ every 1.day, :at => '1:00 am' do
   rake '-s sitemap:refresh'
 end
 
-# every 1.hour do
-#   rake 'sync:public_featured_events'
-# end
+every 1.day, :at => '1:00 am' do
+  rake 'backup:db'
+end
 
-# every 1.day, :at => '1:00 am' do
-#   rake 'backup:db'
-# end
-#
-# every 1.day, :at => '2:00 am' do
-#   rake 'staging:db_clone'
-# end
+every 1.day, :at => '2:00 am' do
+  rake 'staging:db_clone'
+end
