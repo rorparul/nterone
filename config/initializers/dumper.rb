67b565b9d5c90d9ccd1397aebcc7a8f54c7d6e25
Ryan Storberg <ryan@storberg.net>
2016-12-14 20:51:45 -0500

    Add conditionals to Dumber

--- config/initializers/dumper.rb
+++ config/initializers/dumper.rb
@@ -1,3 +1,11 @@
-Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
-  Rails.env.production?
+Dumper::Agent.start_if(app_key: 'YRNUdfrysGblchEd5h83Yj') do
+  Rails.env.production? && Socket.gethostname == 'www.nterone.com'
 end
+
+Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
+  Rails.env.production? && Socket.gethostname == 'nterone.la'
+end
+
+# Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
+#   Rails.env.production? && Socket.gethostname == 'www.nterone.ca'
+# end
