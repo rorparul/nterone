d26ab27aaa7ca28ab4b12410c8d71264d98cc3f1
Ryan Storberg <ryan@storberg.net>
2017-01-10 16:43:57 -0500

    Hotfix

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -2,7 +2,7 @@ module CourseHelper
   def grouped_courses_options(courses)
     groups = courses.group_by { |course| course.platform }
     groups.collect do |group|
-      [group.first.title, group.second.collect { |course| ["#{course.full_title}", course.id] }]
+      [group.first.try(:title), group.second.collect { |course| ["#{course.full_title}", course.id] }]
     end
   end
 end
