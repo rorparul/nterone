4478de2063e812bce3df492352f88b9c3201390b
Ryan Storberg <ryan@storberg.net>
2017-01-16 18:52:49 -0500

    Hotfix

--- db/migrate/20161207011308_add_stuff_to_companies.rb
+++ db/migrate/20161207011308_add_stuff_to_companies.rb
@@ -1,6 +1,6 @@
 class AddStuffToCompanies < ActiveRecord::Migration
   def change
-    add_column(:companies, :user_id, :integer) unless column_exists? :companies, :user_id
+    add_column :companies, :user_id,  :integer
     add_column :companies, :kind,     :string
     add_column :companies, :street,   :string
     add_column :companies, :city,     :string
