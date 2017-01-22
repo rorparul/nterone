076073da4f1e8ed2990dbfecbb6729d9d7007968
Oleg A. Bavaev <jesoba7@gmail.com>
2017-01-23 06:25:01 +1000

    Add deploy task rails:console

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,3 +53,17 @@ namespace :deploy do
 
   before "deploy:migrate", "deploy:db:load"
 end
+
+namespace :rails do
+  desc 'Open a rails console `cap [staging] rails:console [server_index default: 0]`'
+  task :console do
+    on roles(:app) do |server|
+      server_index = ARGV[2].to_i
+      return if server != roles(:app)[server_index]
+      puts "Opening a console on: #{host}...."
+      cmd = "ssh #{server.user}@#{host} -t 'cd #{fetch(:deploy_to)}/current && ~/.rvm/bin/rvm #{fetch(:rvm_ruby_version)} do bundle exec rails console #{fetch(:rails_env)}'"
+      puts cmd
+      exec cmd
+    end
+  end
+end
