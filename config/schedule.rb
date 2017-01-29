bb1c1c8dc0309d8a1e7b6461d4042371fc6be605
Ryan Storberg <ryan@storberg.net>
2017-01-29 16:19:17 -0500

    Comment out clone to staging cron job

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,6 +6,6 @@ every 1.day, :at => '1:00 am' do
   rake 'backup:db'
 end
 
-every 1.day, :at => '2:00 am' do
-  rake 'staging:db_clone'
-end
+# every 1.day, :at => '2:00 am' do
+#   rake 'staging:db_clone'
+# end
