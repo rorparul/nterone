bfea795ddb753cb674c04a4c831e6c85a4085dd3
Ryan Storberg <ryan@storberg.net>
2017-02-07 04:30:37 -0500

    Uncomment out migration files

--- db/migrate/20161220181550_create_opportunities.rb
+++ db/migrate/20161220181550_create_opportunities.rb
@@ -1,16 +1,16 @@
 class CreateOpportunities < ActiveRecord::Migration
   def change
-    create_table :opportunities do |t|
-      t.integer :employee_id
-      t.integer :customer_id
-      t.belongs_to :company
-      t.string :title
-      t.integer :stage
-      t.decimal :amount, precision: 8, scale: 2, default: 0.0
-      t.string :kind
-      t.string :reason_for_loss
-
-      t.timestamps null: false
-    end
+    # create_table :opportunities do |t|
+    #   t.integer :employee_id
+    #   t.integer :customer_id
+    #   t.belongs_to :company
+    #   t.string :title
+    #   t.integer :stage
+    #   t.decimal :amount, precision: 8, scale: 2, default: 0.0
+    #   t.string :kind
+    #   t.string :reason_for_loss
+    #
+    #   t.timestamps null: false
+    # end
   end
 end
