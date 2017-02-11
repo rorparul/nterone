f336c292def6a35df058cefa50c475a36492bca4
Ryan Storberg <ryan@storberg.net>
2017-02-10 19:04:24 -0500

    fix la migrations

--- db/migrate/20161207011308_add_stuff_to_companies.rb
+++ db/migrate/20161207011308_add_stuff_to_companies.rb
@@ -1,12 +1,12 @@
 class AddStuffToCompanies < ActiveRecord::Migration
   def change
-    # add_column :companies, :user_id,  :integer
-    # add_column :companies, :kind,     :string
-    # add_column :companies, :street,   :string
-    # add_column :companies, :city,     :string
-    # add_column :companies, :state,    :string
-    # add_column :companies, :zip_code, :string
-    # add_column :companies, :phone,    :string
-    # add_column :companies, :website,  :string
+    add_column :companies, :user_id,  :integer
+    add_column :companies, :kind,     :string
+    add_column :companies, :street,   :string
+    add_column :companies, :city,     :string
+    add_column :companies, :state,    :string
+    add_column :companies, :zip_code, :string
+    add_column :companies, :phone,    :string
+    add_column :companies, :website,  :string
   end
 end
--- db/migrate/20161207211721_add_parent_to_company.rb
+++ db/migrate/20161207211721_add_parent_to_company.rb
@@ -1,5 +1,5 @@
 class AddParentToCompany < ActiveRecord::Migration
   def change
-    # add_column :companies, :parent_id, :integer
+    add_column :companies, :parent_id, :integer
   end
 end
