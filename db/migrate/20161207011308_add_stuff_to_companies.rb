229e06c43e0e614ce5796e4bbc0a903315a80cb6
Ryan Storberg <ryan@storberg.net>
2017-02-10 20:02:18 -0500

    fix la migrations

--- db/migrate/20161207011308_add_stuff_to_companies.rb
+++ db/migrate/20161207011308_add_stuff_to_companies.rb
@@ -1,5 +1,5 @@
 class AddStuffToCompanies < ActiveRecord::Migration
-  def change
+  def up
     add_column :companies, :user_id,  :integer
     add_column :companies, :kind,     :string
     add_column :companies, :street,   :string
@@ -8,5 +8,8 @@ class AddStuffToCompanies < ActiveRecord::Migration
     add_column :companies, :zip_code, :string
     add_column :companies, :phone,    :string
     add_column :companies, :website,  :string
+  end
+
+  def down
   end
 end
