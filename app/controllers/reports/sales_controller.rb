15e5890b092b46e715bd1ce6f850e0892656e361
Ryan Storberg <ryan@storberg.net>
2017-02-15 20:58:23 -0500

    Fix Opportunities report

--- app/controllers/reports/sales_controller.rb
+++ app/controllers/reports/sales_controller.rb
@@ -9,7 +9,9 @@ class Reports::SalesController < ApplicationController
   def create
     start_date = parse_date_select(report_params, :start_date)
     end_date = parse_date_select(report_params, :end_date)
-    @opportunities = Opportunity.where(date_closed: start_date..end_date).where.not(employee_id: nil, customer_id: nil, date_closed: nil).group_by {|opportunity| opportunity.employee}
+    @company_sales = Opportunity.where(date_closed: start_date..end_date, employee_id: nil).where.not(customer_id: nil)
+    @opportunities = Opportunity.where(date_closed: start_date..end_date).where.not(employee_id: nil, customer_id: nil)
+    @grouped_opportunities = @opportunities.group_by {|opportunity| opportunity.employee}
     respond_to do |format|
       format.xlsx
     end
