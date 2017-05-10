45d014d6788572587b906a6fb819e1fcf696d8a9
Ryan Storberg <ryan@storberg.net>
2017-05-10 14:04:38 -0400

    Add region settings config file

---
+++ config/initializers/region_settings.rb
@@ -0,0 +1,11 @@
+::TopLevelDomain      = `hostname`.match(/\w+$/).to_s
+::RegionalPhoneNumber = case TopLevelDomain
+                        when 'ca'
+                          '613-317-2480'
+                        when 'com'
+                          '703-991-5185'
+                        when 'la'
+                          '+507 6924-8051'
+                        else
+                          '123-456-7890'
+                        end
