3ebef7722870acaa6c04d0ddce355ec774a6c08c
Oleg Bavaev <csolg7@gmail.com>
2017-05-20 13:04:52 +1000

    Change migration set_active_regions

--- db/migrate/20170519023343_set_active_regions_for_united_states.rb
+++ db/migrate/20170519023343_set_active_regions_for_united_states.rb
@@ -5,11 +5,15 @@ class SetActiveRegionsForUnitedStates < ActiveRecord::Migration
         klass = table.singularize.camelize.constantize
       rescue => exception
       else
+        # set active_regions
         if klass.column_names.include?("active_regions")
-          klass.unscoped.where("active_regions @> ?", '{""}').each do |record|
-            record.active_regions = "{#{klass.origin_regions.key(0)}}"
-            record.save
-          end
+          p klass.unscoped.where("active_regions = ? or active_regions = ?", '{}', '{""}').count
+          klass.unscoped.where("active_regions = ? or active_regions = ?", '{}', '{""}').update_all("active_regions": "{#{klass.origin_regions.key(0)}}")
+        end
+        # set origin_region
+        if klass.column_names.include?("origin_region")
+          p klass.unscoped.where("origin_region = ?", nil).count
+          klass.unscoped.where("origin_region = ?", nil).update_all("origin_region": 0)
         end
       end
     end
