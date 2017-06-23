4542816a1520ae77d4892b19b8d489dc3d7acc1b
Ryan Storberg <ryan@storberg.net>
2017-06-23 15:05:54 -0500

    Update resources with sales_force_id

--- app/uploaders/sales_force_uploader.rb
+++ app/uploaders/sales_force_uploader.rb
@@ -60,7 +60,11 @@ class SalesForceUploader
     row.map do |title|
       if type == "Companies"
         case title
+        when "Id"
+          :sales_force_id
         when "Account Name"
+          :title
+        when "Name"
           :title
         when "Account Owner"
           # Must Find By Name
@@ -72,6 +76,8 @@ class SalesForceUploader
         end
       elsif type == "Contacts"
         case title
+        when "Id"
+          :sales_force_id
         when "Account Name"
           :company_name
         when "Mobile"
@@ -87,6 +93,10 @@ class SalesForceUploader
           :first_name
         when "Last Name"
           :last_name
+        when "FirstName"
+          :first_name
+        when "LastName"
+          :last_name
         when "Mailing State/Province"
           :state
         when "Phone"
@@ -96,6 +106,8 @@ class SalesForceUploader
         end
       elsif type == "Leads"
         case title
+        when "Id"
+          :sales_force_id
         when "Company / Account"
           :company_name
         when "Email"
@@ -105,6 +117,10 @@ class SalesForceUploader
         when "First Name"
           :first_name
         when "Last Name"
+          :last_name
+        when "FirstName"
+          :first_name
+        when "LastName"
           :last_name
         when "Lead Owner"
           # Must find by name
@@ -174,101 +190,119 @@ class SalesForceUploader
       row_original.delete(:DELETE)
       case type
       when "Companies"
-        if row_original[:user_id]
-          #  Must find by full name
-          if row_original[:user_id] == "Company Earnings"
-            row_original[:user_id] = nil
-          else
+        # if row_original[:user_id]
+        #   #  Must find by full name
+        #   if row_original[:user_id] == "Company Earnings"
+        #     row_original[:user_id] = nil
+        #   else
+        #
+        #     # NOTE: Capture sales rep
+        #     first_name = row_original[:user_id].split.first
+        #     last_name  = row_original[:user_id].split.last
+        #     users      = User.where(first_name: first_name, last_name: last_name)
+        #     users.each do |user|
+        #       @user = user if user.sales? || user.admin?
+        #     end
+        #
+        #     if @user
+        #       row_original[:user_id] = @user.id
+        #     # elsif @user.nil? && !(first_name.nil?) && !(first_name.blank?) && !(last_name.nil?) && !(last_name.blank?)
+        #     #   email     = first_name + "." + last_name + "@nterone.com"
+        #     #   password  = first_name.first(3) + last_name.first(3) + "Password1"
+        #     #   user      = User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
+        #     #   Role.create(user_id: user.id, role: 3)
+        #     #   row_original[:user_id] = user.id
+        #     # else
+        #     #   # NOTE: This condition needs to be commented back in for the uploader to save the user_id as nil
+        #     #   # NOTE: However, this may result in the creation of pre-existing companies since the row_original will not match those in the database
+        #     #   # NOTE: IMO the best solution is to update all current records where user_id == 0 to user_id == nil before commenting out that line
+        #     #   row_original[:user_id] = nil
+        #     end
+        #   end
+        # end
+        # Company.create(row_original) if Company.where(row_original).empty?
 
-            # NOTE: Capture sales rep
-            first_name = row_original[:user_id].split.first
-            last_name  = row_original[:user_id].split.last
-            users      = User.where(first_name: first_name, last_name: last_name)
-            users.each do |user|
-              @user = user if user.sales? || user.admin?
-            end
 
-            if @user
-              row_original[:user_id] = @user.id
-            # elsif @user.nil? && !(first_name.nil?) && !(first_name.blank?) && !(last_name.nil?) && !(last_name.blank?)
-            #   email     = first_name + "." + last_name + "@nterone.com"
-            #   password  = first_name.first(3) + last_name.first(3) + "Password1"
-            #   user      = User.create!(first_name: first_name, last_name: last_name, email: email, password: password)
-            #   Role.create(user_id: user.id, role: 3)
-            #   row_original[:user_id] = user.id
-            # else
-            #   # NOTE: This condition needs to be commented back in for the uploader to save the user_id as nil
-            #   # NOTE: However, this may result in the creation of pre-existing companies since the row_original will not match those in the database
-            #   # NOTE: IMO the best solution is to update all current records where user_id == 0 to user_id == nil before commenting out that line
-            #   row_original[:user_id] = nil
-            end
-          end
-        end
-        Company.create(row_original) if Company.where(row_original).empty?
+
+        # NOTE: ADD SALESFORCE IDS
+        company = Company.find_by(title: row_original[:title]) if row_original[:title].present?
+        company.update(sales_force_id: row_original[:sales_force_id]) if company.present?
       when "Contacts"
-        # if row_original[:do_not_email]
-          # row_original[:do_not_email] == "1" || 1 ? row_original[:do_not_email] = true : row_original[:do_not_email] = false
+        # # if row_original[:do_not_email]
+        #   # row_original[:do_not_email] == "1" || 1 ? row_original[:do_not_email] = true : row_original[:do_not_email] = false
+        # # end
+        # # If Contact has seller_id obtain information to create an associated lead resource
+        #
+        # # NOTE: Capture sales rep
+        # if row_original[:seller_id]
+        #   first_name = row_original[:seller_id].split.first
+        #   last_name  = row_original[:seller_id].split.last
+        #   users      = User.where(first_name: first_name, last_name: last_name)
+        #   users.each do |user|
+        #     @rep = user if user.sales? || user.admin?
+        #   end
         # end
-        # If Contact has seller_id obtain information to create an associated lead resource
-
-        # NOTE: Capture sales rep
-        if row_original[:seller_id]
-          first_name = row_original[:seller_id].split.first
-          last_name  = row_original[:seller_id].split.last
-          users      = User.where(first_name: first_name, last_name: last_name)
-          users.each do |user|
-            @rep = user if user.sales? || user.admin?
-          end
-        end
-
-        # Create user if user does not exist
-        user = User.find_by(email: row_original[:email])
-        if user.nil? && !(row_original[:email].nil?) && !(row_original[:email].blank?) && !(row_original[:email].empty?) && !(row_original[:first_name].nil?) && !(row_original[:last_name].nil?)
-          row_original.delete(:seller_id)
-          row_original[:parent_id]  = @rep.try(:id)
-          row_original[:password]   = row_original[:first_name].first(3) + row_original[:last_name].first(3) + "Password1"
-          row_original[:status]     = 3
-          row_original[:company_id] = Company.find_by(title: row_original[:company_name]).id unless Company.find_by(title: row_original[:company_name]).nil?
-          # row_original[:first_name].capitalize! if row_original[:first_name].present?
-          # row_original[:last_name].capitalize! if row_original[:last_name].present?
-          user = User.new(row_original)
-          user.save(:validate => false)
-          Role.create(user_id: user.id, role: 4)
-        end
-        #  Create associated lead
-        # if user && @rep
-        #   # Lead.create(seller_id: @rep.id, buyer_id: user.id, status: 'assigned')
-        # else
-        #   email     = first_name + "." + last_name + "@nterone.com"
-        #   password  = first_name.first(3) + last_name.first(3) + "Password1"
-        #   @rep      = User.create(first_name: first_name, last_name: last_name, email: email, password: password)
-        #   Role.create(user_id: @rep.id, role: 3)
-        #   # Lead.create(seller_id: @rep.id, buyer_id: user.id, status: 'assigned')
+        #
+        # # Create user if user does not exist
+        # user = User.find_by(email: row_original[:email])
+        # if user.nil? && !(row_original[:email].nil?) && !(row_original[:email].blank?) && !(row_original[:email].empty?) && !(row_original[:first_name].nil?) && !(row_original[:last_name].nil?)
+        #   row_original.delete(:seller_id)
+        #   row_original[:parent_id]  = @rep.try(:id)
+        #   row_original[:password]   = row_original[:first_name].first(3) + row_original[:last_name].first(3) + "Password1"
+        #   row_original[:status]     = 3
+        #   row_original[:company_id] = Company.find_by(title: row_original[:company_name]).id unless Company.find_by(title: row_original[:company_name]).nil?
+        #   # row_original[:first_name].capitalize! if row_original[:first_name].present?
+        #   # row_original[:last_name].capitalize! if row_original[:last_name].present?
+        #   user = User.new(row_original)
+        #   user.save(:validate => false)
+        #   Role.create(user_id: user.id, role: 4)
         # end
+        # #  Create associated lead
+        # # if user && @rep
+        # #   # Lead.create(seller_id: @rep.id, buyer_id: user.id, status: 'assigned')
+        # # else
+        # #   email     = first_name + "." + last_name + "@nterone.com"
+        # #   password  = first_name.first(3) + last_name.first(3) + "Password1"
+        # #   @rep      = User.create(first_name: first_name, last_name: last_name, email: email, password: password)
+        # #   Role.create(user_id: @rep.id, role: 3)
+        # #   # Lead.create(seller_id: @rep.id, buyer_id: user.id, status: 'assigned')
+        # # end
+
+
+
+        # NOTE: ADD SALESFORCE IDS
+        user = User.find_by(first_name: row_original[:first_name], last_name: row_original[:last_name]) if row_original[:first_name].present? && row_original[:last_name].present?
+        user.update(sales_force_id: row_original[:sales_force_id]) if user.present?
       when "Leads"
-        # NOTE: Capture sales rep
-        if row_original[:seller_id]
-          first_name = row_original[:seller_id].split.first
-          last_name  = row_original[:seller_id].split.last
-          users      = User.where(first_name: first_name, last_name: last_name)
-          users.each do |user|
-            @rep = user if user.sales? || user.admin?
-          end
-        end
+        # # NOTE: Capture sales rep
+        # if row_original[:seller_id]
+        #   first_name = row_original[:seller_id].split.first
+        #   last_name  = row_original[:seller_id].split.last
+        #   users      = User.where(first_name: first_name, last_name: last_name)
+        #   users.each do |user|
+        #     @rep = user if user.sales? || user.admin?
+        #   end
+        # end
+        #
+        # user = User.find_by(email: row_original[:email])
+        # if user.nil? && !(row_original[:email].nil?) && !(row_original[:email].blank?) && !(row_original[:email].empty?) && !(row_original[:first_name].nil?) && !(row_original[:last_name].nil?)
+        #   row_original.delete(:seller_id)
+        #   row_original[:parent_id]  = @rep.try(:id)
+        #   row_original[:password]   = row_original[:first_name].first(3) + row_original[:last_name].first(3) + "Password1"
+        #   row_original[:status]     = 0
+        #   row_original[:company_id] = Company.find_by(title: row_original[:company_name]).id unless Company.find_by(title: row_original[:company_name]).nil?
+        #   # row_original[:first_name].capitalize! if row_original[:first_name].present?
+        #   # row_original[:last_name].capitalize! if row_original[:last_name].present?
+        #   user = User.new(row_original)
+        #   user.save(:validate => false)
+        #   Role.create(user_id: user.id, role: 4)
+        # end
 
-        user = User.find_by(email: row_original[:email])
-        if user.nil? && !(row_original[:email].nil?) && !(row_original[:email].blank?) && !(row_original[:email].empty?) && !(row_original[:first_name].nil?) && !(row_original[:last_name].nil?)
-          row_original.delete(:seller_id)
-          row_original[:parent_id]  = @rep.try(:id)
-          row_original[:password]   = row_original[:first_name].first(3) + row_original[:last_name].first(3) + "Password1"
-          row_original[:status]     = 0
-          row_original[:company_id] = Company.find_by(title: row_original[:company_name]).id unless Company.find_by(title: row_original[:company_name]).nil?
-          # row_original[:first_name].capitalize! if row_original[:first_name].present?
-          # row_original[:last_name].capitalize! if row_original[:last_name].present?
-          user = User.new(row_original)
-          user.save(:validate => false)
-          Role.create(user_id: user.id, role: 4)
-        end
+
+
+        # NOTE: ADD SALESFORCE IDS
+        user = User.find_by(first_name: row_original[:first_name], last_name: row_original[:last_name]) if row_original[:first_name].present? && row_original[:last_name].present?
+        user.update(sales_force_id: row_original[:sales_force_id]) if user.present?
       when "Opportunities"
         if row_original[:account_id]
           company = Company.find_by(title: row_original[:account_id])
