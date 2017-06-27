b72fe264336f68c813149873772c588bf04ffd8b
Ryan Storberg <ryan@storberg.net>
2017-06-27 13:54:41 -0500

    Distinguish between marketing phone numbers and normal phone numbers

--- config/initializers/region_settings.rb
+++ config/initializers/region_settings.rb
@@ -1,9 +1,21 @@
 ::TopLevelDomain      = `hostname`.match(/\w+$/).to_s
+
+::RegionalPhoneNumberForOnlineMarketing = case TopLevelDomain
+                                          when 'ca'
+                                            '613-317-2480'
+                                          when 'com'
+                                            '703-991-5185'
+                                          when 'la'
+                                            '+507 6924-8051'
+                                          else
+                                            '123-456-7890'
+                                          end
+
 ::RegionalPhoneNumber = case TopLevelDomain
                         when 'ca'
                           '613-317-2480'
                         when 'com'
-                          '703-991-5185'
+                          '703-972-2288'
                         when 'la'
                           '+507 6924-8051'
                         else
