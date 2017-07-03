b6a0c29864b624b091c17d5c56c1a734f71e6744
Ryan Storberg <ryan@storberg.net>
2017-07-02 21:19:07 -0500

    Comment out origin_region setter migration that checks with null

--- db/migrate/20170519023343_set_active_regions_for_united_states.rb
+++ db/migrate/20170519023343_set_active_regions_for_united_states.rb
@@ -1,21 +1,21 @@
-# class SetActiveRegionsForUnitedStates < ActiveRecord::Migration
-#   def change
-#     ActiveRecord::Base.connection.tables.each do |table|
-#       begin
-#         klass = table.singularize.camelize.constantize
-#       rescue => exception
-#       else
-#         # set active_regions
-#         if klass.column_names.include?("active_regions")
-#           p klass.unscoped.where("active_regions = ? or active_regions = ?", '{}', '{""}').count
-#           klass.unscoped.where("active_regions = ? or active_regions = ?", '{}', '{""}').update_all("active_regions": "{#{klass.origin_regions.key(0)}}")
-#         end
-#         # set origin_region
-#         if klass.column_names.include?("origin_region")
-#           p klass.unscoped.where("origin_region = ?", nil).count
-#           klass.unscoped.where("origin_region = ?", nil).update_all("origin_region": 0)
-#         end
-#       end
-#     end
-#   end
-# end
+class SetActiveRegionsForUnitedStates < ActiveRecord::Migration
+  # def change
+  #   ActiveRecord::Base.connection.tables.each do |table|
+  #     begin
+  #       klass = table.singularize.camelize.constantize
+  #     rescue => exception
+  #     else
+  #       # set active_regions
+  #       if klass.column_names.include?("active_regions")
+  #         p klass.unscoped.where("active_regions = ? or active_regions = ?", '{}', '{""}').count
+  #         klass.unscoped.where("active_regions = ? or active_regions = ?", '{}', '{""}').update_all("active_regions": "{#{klass.origin_regions.key(0)}}")
+  #       end
+  #       # set origin_region
+  #       if klass.column_names.include?("origin_region")
+  #         p klass.unscoped.where("origin_region = ?", nil).count
+  #         klass.unscoped.where("origin_region = ?", nil).update_all("origin_region": 0)
+  #       end
+  #     end
+  #   end
+  # end
+end
--- db/migrate/20170610075148_set_origin_region_to_united_states.rb
+++ db/migrate/20170610075148_set_origin_region_to_united_states.rb
@@ -1,16 +1,16 @@
-# class SetOriginRegionToUnitedStates < ActiveRecord::Migration
-#   def change
-#     ActiveRecord::Base.connection.tables.each do |table|
-#       begin
-#         klass = table.singularize.camelize.constantize
-#       rescue => exception
-#       else
-#         # set origin_region
-#         if klass.column_names.include?("origin_region")
-#           p klass.unscoped.where("origin_region is null").count
-#           klass.unscoped.where("origin_region is null").update_all("origin_region": 0)
-#         end
-#       end
-#     end
-#   end
-# end
+class SetOriginRegionToUnitedStates < ActiveRecord::Migration
+  # def change
+  #   ActiveRecord::Base.connection.tables.each do |table|
+  #     begin
+  #       klass = table.singularize.camelize.constantize
+  #     rescue => exception
+  #     else
+  #       # set origin_region
+  #       if klass.column_names.include?("origin_region")
+  #         p klass.unscoped.where("origin_region is null").count
+  #         klass.unscoped.where("origin_region is null").update_all("origin_region": 0)
+  #       end
+  #     end
+  #   end
+  # end
+end
