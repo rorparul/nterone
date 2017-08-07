dc8da9fa7239bce97ce705592f0804b0ec0baf47
Ryan Storberg <ryan@storberg.net>
2017-08-07 13:39:15 -0500

    Fix NterOne.la's DB backup process

--- config/initializers/dumper.rb
+++ config/initializers/dumper.rb
@@ -3,7 +3,7 @@ Dumper::Agent.start_if(app_key: 'YRNUdfrysGblchEd5h83Yj') do
 end
 
 Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
-  Rails.env.production? && Socket.gethostname == 'nterone.la'
+  Rails.env.production? && Socket.gethostname == 'nterone-la'
 end
 
 Dumper::Agent.start_if(app_key: 'UnBZTswiW0ZTVu4EWww1Gz') do
