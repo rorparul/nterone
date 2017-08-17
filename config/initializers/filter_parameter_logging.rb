e7e6779b0174409891ddf15c118e4dee3dcc3afe
Oleg Bavaev <csolg7@gmail.com>
2017-08-17 20:29:35 +0500

    !Danger add credit card params to filter from log

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+# Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :credit_card_number, :expiration_month, :expiration_year, :security_code]
