2c96b51a4ef1925c0de9d3567ec2f319f2749c34
Oleg Bavaev <csolg7@gmail.com>
2017-08-23 01:55:03 +0500

    Add angular signature email directive

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -92,4 +92,13 @@ module ApplicationHelper
       ["Canada", canadian_provinces]
     ]
   end
+
+  def angular_templates templates_path
+    templates = Dir[templates_path + '**/*'].select { |f| File.file? f }
+    templates.inject("") do |js, template|
+      rendered = render file: template
+      template_id = Pathname.new(template.gsub(/\.(\w+)$/, '')).relative_path_from(Pathname.new(templates_path))
+      js += "<script type='text/ng-template' id='#{template_id.to_s}'>#{rendered}</script>"
+    end
+  end
 end
