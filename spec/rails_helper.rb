000ed73b6a24795593612e429efe46d33eaa3f23
Oleg Bavaev <csolg7@gmail.com>
2017-09-08 12:51:58 +0500

    Remove duplicate vcr configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,18 +27,3 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 end
 
-# Automatically record all remote calls (with RSpec)
-RSpec.configure do |config|
-  # Add VCR to all tests
-  config.around(:each) do |example|
-    options = example.metadata[:vcr] || {}
-    if options[:record] == :skip
-      VCR.turned_off(&example)
-    else
-      name = example.metadata[:full_description].split(/\s+/, 2).join('/').underscore.gsub(/\./,'/').gsub(/[^\w\/]+/, '_').gsub(/\/$/, '')
-      VCR.use_cassette(name, options, &example)
-    end
-  end
-end
-
-
