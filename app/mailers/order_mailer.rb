2b537a822a29a3d929ca5076dfcc885c89337842
Ryan Storberg <ryan@storberg.net>
2017-10-09 14:24:36 -0500

    Troubleshoot

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -10,6 +10,16 @@ class OrderMailer < ApplicationMailer
       la: 'marketing360+m10794@bcc.mad360.net'
     }
 
+    attachments['nterone_receipt.pdf'] = WickedPdf.new.pdf_from_string(
+      render_to_string(
+        pdf: 'NterOne Receipt',
+        margin: { bottom: 32 },
+        template: 'orders/receipt.html.slim',
+        locals: { order: @order },
+        footer:  { html: { template: 'layouts/_footer.html.slim' } }
+      )
+    )
+
     mail(
       to: @user.email,
       bcc: [
@@ -21,15 +31,6 @@ class OrderMailer < ApplicationMailer
       ],
       subject: "NterOne.#{@tld} Order Confirmation"
     )
-      .attachments['nterone_receipt.pdf'] = WickedPdf.new.pdf_from_string(
-        render_to_string(
-          pdf: 'NterOne Receipt',
-          margin: { bottom: 32 },
-          template: 'orders/receipt.html.slim',
-          locals: { order: @order },
-          footer:  { html: { template: 'layouts/_footer.html.slim' } }
-        )
-      )
   end
 
   def lab_rental_notification(user, order_pods)
@@ -54,3 +55,18 @@ class OrderMailer < ApplicationMailer
     end
   end
 end
+
+# class QuoteMailer < ApplicationMailer
+#   def pdf_attachment(lead)
+#     @lead = lead
+#
+#     mail(to: @lead.buyer.email, subject: "Your NterOne.com Sales Quotation")
+#       .attachments["quote.pdf"] = WickedPdf.new.pdf_from_string(
+#         render_to_string(pdf: 'quote',
+#                          margin: { bottom: 32 },
+#                          template: 'leads/quote.html.slim',
+#                          locals: { lead: @lead },
+#                          footer:  { html: { template:'layouts/_footer.html.slim'}})
+#       )
+#   end
+# end
