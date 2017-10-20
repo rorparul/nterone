cbf3de0f7c272bf42cc594fad711590fa8dc3832
Oleg Bavaev <csolg7@gmail.com>
2017-10-21 06:50:41 +1000

    Show report for selected month

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -4,22 +4,24 @@ class Admin::SalesController < Admin::BaseController
     if params[:date]
       month = params[:date][:month].to_i
       year = params[:date][:year].to_i
-      selected_month = Date.new(year, month)
+      @selected_month = Date.new(year, month)
     else
-      selected_month = Date.today
+      @selected_month = Date.today
     end
 
     @total_amount = Opportunity.unscoped.
-      where(date_closed: selected_month.beginning_of_month..selected_month.end_of_month).
+      where(date_closed: @selected_month.beginning_of_month..@selected_month.end_of_month).
       sum('amount')
 
-    @region_amount = {}
+    @region_amounts = {}
+    @region_percents = {}
     Event.origin_regions.each do |region, region_value|
       amount = Opportunity.unscoped.
         where(origin_region: region_value).
-        where(date_closed: selected_month.beginning_of_month..selected_month.end_of_month).
+        where(date_closed: @selected_month.beginning_of_month..@selected_month.end_of_month).
         sum('amount')
-      @region_amount[region_value] = (amount / @total_amount * 100).round
+      @region_percents[region_value] = (amount / @total_amount * 100).round
+      @region_amounts[region_value] = amount
     end
   end
 
