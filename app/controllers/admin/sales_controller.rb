b22a92a6bc2507405cf526cdc31ac0e1033f9806
Oleg Bavaev <csolg7@gmail.com>
2017-10-27 13:08:35 +1000

    Add sales details to reports

--- app/controllers/admin/sales_controller.rb
+++ app/controllers/admin/sales_controller.rb
@@ -25,4 +25,43 @@ class Admin::SalesController < Admin::BaseController
     end
   end
 
+  def details
+    if params[:report]
+      @started_at = parse_date_select(report_params, :started_at)
+      @ended_at = parse_date_select(report_params, :ended_at)
+    end
+
+    @started_at ||= Date.today.beginning_of_month
+    @ended_at ||= Date.today.end_of_month
+
+    opportunities_scope = Opportunity.where(date_closed: @started_at..@ended_at)
+    if params[:report]
+      opportunities_scope = opportunities_scope.pending if params[:report][:status] == "open"
+      opportunities_scope = opportunities_scope.waiting if params[:report][:status] == "waiting"
+      opportunities_scope = opportunities_scope.won     if params[:report][:status] == "won"
+    end
+
+    @opportunities_filtered_sum = opportunities_scope.sum('amount')
+
+    smart_listing_create(
+      :opportunities,
+      opportunities_scope,
+      partial: 'admin/sales/listing',
+      # sort_attributes: [
+      #   [:created_at, 'opportunities.created_at'],
+      #   [:stage, 'stage'],
+      #   [:waiting, 'waiting'],
+      #   [:date_closed, 'date_closed'],
+      #   [:amount, 'amount']
+      # ],
+      default_sort: { date_closed: 'desc' }
+    )
+  end
+
+  private
+
+    def report_params
+      params.require(:report)
+    end
+
 end
