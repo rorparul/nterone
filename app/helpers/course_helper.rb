a404a45d6d78d6d356abe14e1f4b64cc73a46ac2
Ryan Storberg <ryan@storberg.net>
2017-10-31 15:18:30 -0600

    Add active regions tags to course dropdown for opportunities

--- app/helpers/course_helper.rb
+++ app/helpers/course_helper.rb
@@ -2,7 +2,17 @@ module CourseHelper
   def grouped_courses_options(courses)
     groups = courses.group_by { |course| course.platform }
     groups.collect do |group|
-      [group.first.try(:title), group.second.collect { |course| ["#{course.full_title}", course.id] }]
+      [
+        group.first.try(:title), group.second.collect do |course|
+          ["#{course.full_title} #{active_regions_tags(course)}", course.id]
+        end
+      ]
     end
+  end
+
+  def active_regions_tags(course)
+    tags = ""
+    course.active_regions.each { |ar| tags += ar.humanize.upcase if ar.present? }
+    tags.present? ? " for #{tags}" : tags
   end
 end
