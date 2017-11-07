242def8cc58bed33055acb660c509b81be06da94
Ryan Storberg <ryan@storberg.net>
2017-11-06 20:15:13 -0500

    Only backup DB from one server

--- config/initializers/dumper.rb
+++ config/initializers/dumper.rb
@@ -2,10 +2,10 @@ Dumper::Agent.start_if(app_key: 'YRNUdfrysGblchEd5h83Yj') do
   Rails.env.production? && Socket.gethostname == 'www.nterone.com'
 end
 
-Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
-  Rails.env.production? && Socket.gethostname == 'nterone-la'
-end
+# Dumper::Agent.start_if(app_key: '9d5Q5cp9FvYLE3goq13KH6') do
+#   Rails.env.production? && Socket.gethostname == 'nterone-la'
+# end
 
-Dumper::Agent.start_if(app_key: 'UnBZTswiW0ZTVu4EWww1Gz') do
-  Rails.env.production? && Socket.gethostname == 'www-nterone-ca'
-end
+# Dumper::Agent.start_if(app_key: 'UnBZTswiW0ZTVu4EWww1Gz') do
+#   Rails.env.production? && Socket.gethostname == 'www-nterone-ca'
+# end
