158870637c480f45bf765b6f17c0353242a4ca2b
Oleg Bavaev <csolg7@gmail.com>
2017-11-30 21:49:34 +1000

    Add storing tld to user settings

---
+++ app/controllers/regions_controller.rb
@@ -0,0 +1,14 @@
+class RegionsController < ApplicationController
+
+  def switch
+    tld = params[:tld]
+
+    if user_signed_in?
+      current_user.settings.tld = tld
+    end
+
+    url = "nterone.#{tld}"
+    redirect_to url
+  end
+
+end
