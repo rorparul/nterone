70a50ea19118bc401920230b72bd0b40b7e3bcf4
Ryan Storberg <ryan@storberg.net>
2017-12-04 17:05:43 -0500

    Update Sitemap file

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -17,19 +17,19 @@ SitemapGenerator::Sitemap.create do
   Article.current_region.find_each do |article|
     add article_path(article), lastmod: article.updated_at
   end
-  Platform.current_region.find_each do |platform|
+  Platform.active.current_region.find_each do |platform|
     add platform_path(platform), lastmod: platform.updated_at
   end
-  Category.current_region.find_each do |category|
+  Category.active.current_region.find_each do |category|
     add platform_category_path(category.platform, category), lastmod: category.updated_at
   end
-  Subject.current_region.find_each do |subject|
+  Subject.active.current_region.find_each do |subject|
     add platform_subject_path(subject.platform, subject), lastmod: subject.updated_at
   end
-  Course.current_region.find_each do |course|
+  Course.active.current_region.find_each do |course|
     add platform_course_path(course.platform, course), lastmod: course.updated_at
   end
-  VideoOnDemand.current_region.find_each do |video_on_demand|
+  VideoOnDemand.active.current_region.find_each do |video_on_demand|
     add platform_video_on_demand_path(video_on_demand.platform, video_on_demand), lastmod: video_on_demand.updated_at
   end
 end
