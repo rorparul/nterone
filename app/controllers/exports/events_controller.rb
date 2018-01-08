d6a1e262c8c8bf9faeb2ce46bde42297373cada4
Ryan Storberg <ryan@storberg.net>
2018-01-07 21:29:40 -0600

    Add events export for Ingram

--- app/controllers/exports/events_controller.rb
+++ app/controllers/exports/events_controller.rb
@@ -6,14 +6,33 @@ class Exports::EventsController < ApplicationController
   end
 
   def create
-    @events = @events.where(clean_params(company_params[:filters]))
-    @events = @events.custom_search(params[:search]) if params[:search].present?
-    render xlsx: 'index', filename: "events-#{DateTime.now}.xlsx"
+    if export_params[:platform_id].blank?
+      events = Event.all
+    else
+      events = Platform.find(export_params[:platform_id]).events
+    end
+
+    events  = events.where('start_date >= ?', export_params_start_date)
+    @events = events.where(public: export_params[:public])
+
+    render filename: "events-#{DateTime.now}.xlsx"
   end
 
   private
 
   def export_params
-    params.require(:export).permit()
+    params.require(:export).permit(
+      :platform_id,
+      :public,
+      :start_date
+    )
+  end
+
+  def export_params_start_date
+    Date.new(
+      export_params['start_date(1i)'].to_i,
+      export_params['start_date(2i)'].to_i,
+      export_params['start_date(3i)'].to_i
+    )
   end
 end
