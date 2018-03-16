317f02aed4cfd616b1de1d209f5e71a7272b5152
Kevin J. Storberg <kevin@storberg.net>
2018-03-16 16:06:27 +0800

    put validation back into company.tb

--- app/models/company.rb
+++ app/models/company.rb
@@ -44,6 +44,8 @@ class Company < ActiveRecord::Base
   has_many :account_opportunities, class_name: 'Opportunity', foreign_key: 'account_id'
   has_many :partner_opportunities, class_name: 'Opportunity', foreign_key: 'partner_id'
 
+  validates :title, :industry_code, presence: true
+
   before_create :create_slug
 
   after_save :reassign_company_users, if: proc { |model| model.user_id_changed? }
