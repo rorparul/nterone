169a0791416e346525e52c96a9c3cfdcd1760c14
Ryan Storberg <ryan@storberg.net>
2018-03-21 18:16:08 +0900

    Change region title to colored badge

--- app/models/concerns/regions.rb
+++ app/models/concerns/regions.rb
@@ -17,9 +17,9 @@ module Regions
     def options_for_regions
       origin_regions.keys.sort.map { |region| [region.titleize, region] }
     end
-    
+
     def options_for_origin_region
-      origin_regions.map{|region, region_value| [region.titleize, region_value]}
+      origin_regions.map {|region, region_value| [region.titleize, region_value]}
     end
 
     def get_session_region
