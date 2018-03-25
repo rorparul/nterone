3748a9c596271469e3dafa18b37e26fef61ee767
Ryan Storberg <ryan@storberg.net>
2018-03-25 23:56:53 +0900

    Started redesign of header

---
+++ app/helpers/shared_helper.rb
@@ -0,0 +1,19 @@
+module SharedHelper
+  def sign_in_or_account
+    unless user_signed_in?
+      text = 'Login'
+      url  = new_user_session_path
+    else
+      text = 'Account'
+      if current_user.admin? || current_user.partner? || current_user.sales?
+        url = admin_classes_path
+      elsif current_user.instructor?
+        url = instructor_classes_path
+      else
+        url = my_account_plan_path
+      end
+    end
+
+    link_to text, url, id: 'sign-in-or-account', class: 'btn btn-blue-gradient', role: 'button'
+  end
+end
