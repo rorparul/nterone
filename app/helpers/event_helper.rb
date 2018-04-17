ac362589cf412c14ba60115fa621e54f5747c118
mohit <mkumaror@gmail.com>
2018-04-17 12:55:09 +0530

    move instructors with 0 daily rate at the bottom

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -13,6 +13,10 @@ module EventHelper
     if selected_instructor.present?
       instructors << selected_instructor
     end
-    instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: $#{number_with_delimiter(number_with_precision(user.daily_rate, precision: 2))}", user.id] }
+    instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: $#{number_with_delimiter(number_with_precision(user.daily_rate, precision: 2))}", user.id] if user.daily_rate > 0}
+
+    instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: $#{number_with_delimiter(number_with_precision(user.daily_rate, precision: 2))}", user.id] if user.daily_rate == 0}
+    
+    instructor_with_price.compact + instructor_without_price.compact
   end
 end
