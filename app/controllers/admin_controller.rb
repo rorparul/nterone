cf62af39c439a630de6e8b826f9a39018a34ba9d
Ryan Storberg <ryan@storberg.net>
2018-04-21 01:56:09 +0900

    Change event color

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -105,7 +105,7 @@ class AdminController < ApplicationController
       format.html
       format.js
       format.json do
-        render json: events_scope.select { |event| event.instructor.present? }.map{ |event| { 'title': event.title_with_instructor_and_state, 'start': event.start_date.strftime("%Y-%m-%d"), 'end': (event.end_date + 1.day).strftime("%Y-%m-%d")} }.to_json
+        render json: events_scope.select { |event| event.instructor.present? }.map{ |event| { 'title': event.title_with_instructor_and_state, 'start': event.start_date.strftime("%Y-%m-%d"), 'end': (event.end_date + 1.day).strftime("%Y-%m-%d"), 'color': '#808080' } }.to_json
       end
     end
   end
