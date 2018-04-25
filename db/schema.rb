5c52dcb3d70bd65edc23c8f833178b59a58103e3
Ryan Storberg <ryan@storberg.net>
2018-04-25 22:24:10 +0900

    Modify platform/show layout

--- db/schema.rb
+++ db/schema.rb
@@ -234,6 +234,7 @@ ActiveRecord::Schema.define(version: 20180423092101) do
     t.string   "subject"
     t.integer  "origin_region"
     t.text     "active_regions", default: [],              array: true
+    t.text     "message",        default: ""
   end
 
   add_index "contact_us_submissions", ["origin_region"], name: "index_contact_us_submissions_on_origin_region", using: :btree
@@ -251,10 +252,10 @@ ActiveRecord::Schema.define(version: 20180423092101) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at",                                                    null: false
-    t.datetime "updated_at",                                                    null: false
+    t.datetime "created_at",                                                      null: false
+    t.datetime "updated_at",                                                      null: false
     t.integer  "platform_id"
-    t.boolean  "active",                                        default: true
+    t.boolean  "active",                                          default: true
     t.string   "abbreviation"
     t.text     "intro"
     t.text     "overview"
@@ -262,18 +263,19 @@ ActiveRecord::Schema.define(version: 20180423092101) do
     t.text     "intended_audience"
     t.string   "pdf"
     t.text     "video_preview"
-    t.decimal  "price",                 precision: 8, scale: 2, default: 0.0
+    t.decimal  "price",                   precision: 8, scale: 2, default: 0.0
     t.string   "slug"
     t.string   "page_title"
     t.text     "page_description"
-    t.boolean  "partner_led",                                   default: false
+    t.boolean  "partner_led",                                     default: false
     t.string   "heading"
-    t.boolean  "satellite_viewable",                            default: true
+    t.boolean  "satellite_viewable",                              default: true
     t.integer  "origin_region"
-    t.text     "active_regions",                                default: [],                 array: true
+    t.text     "active_regions",                                  default: [],                 array: true
     t.string   "cisco_id"
-    t.boolean  "archived",                                      default: false
-    t.decimal  "book_cost_per_student",                         default: 0.0
+    t.boolean  "archived",                                        default: false
+    t.decimal  "book_cost_per_student",                           default: 0.0
+    t.text     "featured_course_summary",                         default: ""
   end
 
   add_index "courses", ["origin_region"], name: "index_courses_on_origin_region", using: :btree
