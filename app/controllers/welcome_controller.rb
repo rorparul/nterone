4cfcf640a2dc8c4f7fc16ee12f00835670db8450
Ryan Storberg <ryan@storberg.net>
2018-05-04 15:38:16 +0900

    Remove marketing phone numbers settings

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,8 +7,6 @@ class WelcomeController < ApplicationController
   private
 
   def set_featured_courses
-    # tld = Rails.application.config.tld
-
     @featured_course_1 = Course.find_by(id: YAML.load(Setting.featured_course_1_id)[$tld])
     @featured_course_2 = Course.find_by(id: YAML.load(Setting.featured_course_2_id)[$tld])
     @featured_course_3 = Course.find_by(id: YAML.load(Setting.featured_course_3_id)[$tld])
