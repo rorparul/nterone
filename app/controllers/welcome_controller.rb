83d6bbdf2d112387b77f902e5b2e24626e7ae03d
Ryan Storberg <ryan@storberg.net>
2018-05-04 15:32:15 +0900

    Troubleshoot featured courses

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,12 +7,12 @@ class WelcomeController < ApplicationController
   private
 
   def set_featured_courses
-    tld = Rails.application.config.tld
+    # tld = Rails.application.config.tld
 
-    @featured_course_1 = Course.find_by(id: YAML.load(Setting.featured_course_1_id)[tld])
-    @featured_course_2 = Course.find_by(id: YAML.load(Setting.featured_course_2_id)[tld])
-    @featured_course_3 = Course.find_by(id: YAML.load(Setting.featured_course_3_id)[tld])
-    @featured_course_4 = Course.find_by(id: YAML.load(Setting.featured_course_4_id)[tld])
+    @featured_course_1 = Course.find_by(id: YAML.load(Setting.featured_course_1_id)[$tld])
+    @featured_course_2 = Course.find_by(id: YAML.load(Setting.featured_course_2_id)[$tld])
+    @featured_course_3 = Course.find_by(id: YAML.load(Setting.featured_course_3_id)[$tld])
+    @featured_course_4 = Course.find_by(id: YAML.load(Setting.featured_course_4_id)[$tld])
     rescue
   end
 end
