c96a4721d9c20f56641b8e8fc83c61b8333caf28
mohit <mkumaror@gmail.com>
2018-05-07 16:24:51 +0530

    change instructor drop down on the basis of event format

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -14,10 +14,17 @@ module EventHelper
     if selected_instructor.present?
       instructors << selected_instructor
     end
+     
+    if event.format.present? && (event.format == "On-site" || event.format == "Live Online")
+      instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) > 0}
 
-    instructor_with_price    = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) > 0}
-    instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) == 0}
+      instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) == 0}
+    else
+      instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) > 0}
 
+      instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) == 0}
+    end
+    
     instructor_with_price.compact + instructor_without_price.compact
   end
 end
