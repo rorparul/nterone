2eb1fe22b2c47dcdbcfa2c0d2797e792947d225f
mohit <mkumaror@gmail.com>
2018-05-11 13:17:23 +0530

    fix instructor selector issue

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -9,22 +9,26 @@ module EventHelper
   end
 
   def instructors_with_rate(instructors, event)
-    selected_instructor = event.instructor
+    if event.present?
+      selected_instructor = event.instructor
 
-    if selected_instructor.present?
-      instructors << selected_instructor
-    end
-     
-    if event.format.present? && (event.format == "On-site" || event.format == "Live Online")
-      instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) > 0}
+      if selected_instructor.present?
+        instructors << selected_instructor
+      end
+       
+      if event.format.present? && (event.format == "On-site" || event.format == "Live Online")
+        instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) > 0}
 
-      instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) == 0}
+        instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) == 0}
+      else
+        instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) > 0}
+
+        instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) == 0}
+      end
+      
+      instructor_with_price.compact + instructor_without_price.compact
     else
-      instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) > 0}
-
-      instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) == 0}
-    end
-    
-    instructor_with_price.compact + instructor_without_price.compact
+      instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] }
+    end 
   end
 end
