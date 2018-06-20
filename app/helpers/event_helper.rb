98782a09dd8c836c0b0c87bde225fe9c1d7adb75
Ryan Storberg <ryan@storberg.net>
2018-06-20 19:20:24 +0700

    Fix duplicate instructor issue

--- app/helpers/event_helper.rb
+++ app/helpers/event_helper.rb
@@ -12,10 +12,10 @@ module EventHelper
     if event.present?
       selected_instructor = event.instructor
 
-      if selected_instructor.present?
+      if selected_instructor.present? && instructors.exclude?(selected_instructor)
         instructors << selected_instructor
       end
-       
+
       if event.format.present? && (event.format == "On-site" || event.format == "Live Online")
         instructor_with_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: #{event.format}: $#{number_with_delimiter(number_with_precision(user.daily_rate(event), precision: 2))}", user.id] if user.daily_rate(event) > 0}
 
@@ -25,10 +25,10 @@ module EventHelper
 
         instructor_without_price = instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] if user.daily_rate(event) == 0}
       end
-      
+
       instructor_with_price.compact + instructor_without_price.compact
     else
       instructors.collect { |user| ["#{user.last_name}, #{user.first_name}: On-site: $#{number_with_delimiter(number_with_precision(user.onsite_daily_rate, precision: 2))}, On-line: $#{number_with_delimiter(number_with_precision(user.online_daily_rate, precision: 2))}", user.id] }
-    end 
+    end
   end
 end
