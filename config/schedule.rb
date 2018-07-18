21257b8622f79a446a9d434cbb1483d156b27252
Oleg Bavaev <csolg7@gmail.com>
2018-07-18 20:38:51 +1000

    Remove sidetiq gem

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,6 +10,10 @@ every 1.hour do
   runner "Cart.notify_that_cart_not_empty"
 end
 
+every 1.day, :at => '2:00 am' do
+  runner "EventReminderWorker.perform_async"
+end
+
 # every 1.day, :at => '2:00 am' do
 #   rake 'staging:db_clone'
 # end
