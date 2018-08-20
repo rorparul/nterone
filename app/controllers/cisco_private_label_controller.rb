780eadd2752680186180d70e1d26e0ae2af85bc7
Ryan Storberg <ryan@storberg.net>
2018-08-21 00:04:49 +0200

    Add CPL API authorization

--- app/controllers/cisco_private_label_controller.rb
+++ app/controllers/cisco_private_label_controller.rb
@@ -1,6 +1,7 @@
 class CiscoPrivateLabelController < ApplicationController
   include CiscoPrivateLabel
 
+  before_action :authorize_user
   before_action :set_order, only: [:cpl_orders_post, :cpl_orders_cancel, :cpl_enrollments_post]
 
   def cpl_log
@@ -17,6 +18,8 @@ class CiscoPrivateLabelController < ApplicationController
   end
 
   def cpl_orders_cancel
+    @order ||= Order.new(id: params[:order_id])
+
     cpl_post_orders_cancel(@order)
     redirect_to cpl_log_path
   end
@@ -32,7 +35,11 @@ class CiscoPrivateLabelController < ApplicationController
 
   private
 
+  def authorize_user
+    redirect_to root_path unless current_user.admin?
+  end
+
   def set_order
-    @order = Order.find(params[:order_id])
+    @order = Order.find_by(id: params[:order_id])
   end
 end
