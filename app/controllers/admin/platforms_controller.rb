8f3d9560df658e589e0d86af5324425dbd374528
Ryan Storberg <ryan@storberg.net>
2018-11-02 02:05:15 +0100

    Show all platforms to developer

--- app/controllers/admin/platforms_controller.rb
+++ app/controllers/admin/platforms_controller.rb
@@ -3,7 +3,11 @@ class Admin::PlatformsController < ApplicationController
   before_action :authorize_admin, only: [:index]
 
   def index
-    @platforms = Platform.active
+    if current_user.developer?
+      @platforms = Platform.all
+    else
+      @platforms = Platform.active
+    end
   end
 
   private
